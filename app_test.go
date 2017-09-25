684c6ffcc7d22d10f0a4784890eb001e49903d02
John Scherff <jscherff@gmail.com>
2017-09-24 21:15:38 -0700

    Testing improvements

--- app_test.go
+++ app_test.go
@@ -27,178 +27,6 @@ import (
 	`github.com/jscherff/gotest`
 )
 
-var (
-	gen1JSON = []byte(
-	`{
-		"host_name": "John-SurfacePro",
-		"vendor_id": "0acd",
-		"product_id": "2030",
-		"vendor_name": "ID TECH",
-		"product_name": "TM3 Magstripe USB-HID Keyboard Reader",
-		"serial_num": "",
-		"software_id": "",
-		"product_ver": "",
-		"bus_number": 1,
-		"bus_address": 14,
-		"port_number": 1,
-		"buffer_size": 0,
-		"max_pkt_size": 8,
-		"usb_spec": "2.00",
-		"usb_class": "per-interface",
-		"usb_subclass": "per-interface",
-		"usb_protocol": "0",
-		"device_speed": "full",
-		"device_ver": "1.00",
-		"object_type": "*usbci.Generic",
-		"device_sn": "",
-		"factory_sn": "",
-		"descriptor_sn": "",
-		"changes": null,
-		"vendor": {}
-	}`)
-
-	gen2JSON = []byte(
-	`{
-		"host_name": "John-SurfacePro",
-		"vendor_id": "0acd",
-		"product_id": "2030",
-		"vendor_name": "ID TECH",
-		"product_name": "TM3 Magstripe USB-HID Keyboard Reader",
-		"serial_num": "",
-		"software_id": "",
-		"product_ver": "",
-		"bus_number": 1,
-		"bus_address": 14,
-		"port_number": 1,
-		"buffer_size": 0,
-		"max_pkt_size": 8,
-		"usb_spec": "2.00",
-		"usb_class": "per-interface",
-		"usb_subclass": "per-interface",
-		"usb_protocol": "0",
-		"device_speed": "full",
-		"device_ver": "1.00",
-		"object_type": "*usbci.Generic",
-		"device_sn": "",
-		"factory_sn": "",
-		"descriptor_sn": "",
-		"changes": null,
-		"vendor": {}
-	}`)
-
-	mag1JSON = []byte(
-	`{
-		"host_name": "John-SurfacePro",
-		"vendor_id": "0801",
-		"product_id": "0001",
-		"vendor_name": "Mag-Tek",
-		"product_name": "USB Swipe Reader",
-		"serial_num": "24F0014",
-		"software_id": "21042818B01",
-		"product_ver": "",
-		"bus_number": 1,
-		"bus_address": 13,
-		"port_number": 1,
-		"buffer_size": 24,
-		"max_pkt_size": 8,
-		"usb_spec": "1.10",
-		"usb_class": "per-interface",
-		"usb_subclass": "per-interface",
-		"usb_protocol": "0",
-		"device_speed": "full",
-		"device_ver": "1.00",
-		"object_type": "*usbci.Magtek",
-		"device_sn": "24F0014",
-		"factory_sn": "",
-		"descriptor_sn": "24F0014",
-		"changes": null,
-		"vendor": {}
-	}`)
-
-	mag2JSON = []byte(
-	`{
-		"host_name": "John-SurfacePro",
-		"vendor_id": "0801",
-		"product_id": "0001",
-		"vendor_name": "Mag-Tek",
-		"product_name": "USB Swipe Reader",
-		"serial_num": "24F0014",
-		"software_id": "21042818B02",
-		"product_ver": "",
-		"bus_number": 1,
-		"bus_address": 13,
-		"port_number": 1,
-		"buffer_size": 24,
-		"max_pkt_size": 8,
-		"usb_spec": "2.00",
-		"usb_class": "per-interface",
-		"usb_subclass": "per-interface",
-		"usb_protocol": "0",
-		"device_speed": "full",
-		"device_ver": "1.00",
-		"object_type": "*usbci.Magtek",
-		"device_sn": "24F0014",
-		"factory_sn": "",
-		"descriptor_sn": "24F0014",
-		"changes": null,
-		"vendor": {}
-	}`)
-
-	mag1ShaJSON = [32]byte{
-		0xaf,0x81,0xad,0x6f,0xf7,0x6c,0x37,0xbd,
-		0x45,0x8c,0xe8,0xfc,0xa5,0xd9,0x06,0x38,
-		0x5b,0xc2,0x80,0x32,0x08,0x53,0x8b,0xac,
-		0x86,0xe0,0x2a,0xdd,0xc9,0x8a,0x7a,0x32,
-	}
-
-	mag1ShaPrettyJSON = [32]byte{
-		0x36,0x54,0xc7,0x2f,0x3e,0xf5,0xe3,0x4d,
-		0xc8,0x67,0x66,0x17,0x27,0x9d,0x0e,0x1a,
-		0xc0,0xde,0x50,0x0d,0x20,0x8e,0x54,0x33,
-		0x00,0x9e,0x17,0x32,0xe1,0x90,0x0a,0xe7,
-	}
-
-	mag1ShaXML = [32]byte{
-		0x82,0xc7,0x14,0x84,0xee,0xb5,0x4a,0x91,
-		0xfc,0x92,0xa6,0x8b,0xeb,0xf7,0xd4,0x66,
-		0x93,0xad,0xc0,0x6b,0x89,0x3e,0x99,0x11,
-		0x28,0xfc,0x7e,0x61,0xf3,0x4f,0x7c,0xed,
-	}
-
-	mag1ShaPrettyXML = [32]byte{
-		0x0f,0x05,0x4e,0x13,0x51,0x5e,0x90,0x9d,
-		0x3d,0x39,0xfb,0xb8,0x6a,0x14,0x20,0xcb,
-		0x3a,0xd0,0xb6,0x79,0xa5,0x56,0xad,0xf7,
-		0xce,0xff,0x31,0xdc,0x56,0x2a,0xbd,0x92,
-	}
-
-	mag1ShaCSV = [32]byte{
-		0x98,0xd5,0xe9,0x1d,0x6f,0xa9,0xe8,0xfe,
-		0x7c,0xd6,0xa8,0xa0,0x7e,0x88,0x48,0xd4,
-		0xcf,0x8b,0x04,0x9c,0x05,0x3e,0x1b,0x58,
-		0x41,0x3c,0xf8,0x3e,0x27,0x8a,0x98,0xea,
-	}
-
-	mag1ShaNVP = [32]byte{
-		0xd0,0xc4,0xea,0x8b,0x3c,0x80,0xae,0x79,
-		0xe8,0x0e,0x17,0x1e,0xd3,0x55,0x09,0x88,
-		0xbb,0x2b,0x11,0x84,0xac,0x3d,0xd9,0x42,
-		0x50,0xc4,0x5d,0x5e,0x70,0xd3,0x65,0xe2,
-	}
-
-	mag1ShaLegacy = [32]byte{
-		0xb3,0xb5,0x58,0x2b,0xb2,0xd9,0x88,0x4a,
-		0x78,0xd5,0xf4,0x2d,0x98,0x0c,0x2b,0x81,
-		0xfd,0xd1,0x43,0xb6,0xcc,0x58,0x14,0x39,
-		0x23,0x30,0x50,0x2f,0xe3,0x59,0x88,0x5a,
-	}
-
-	mag1, mag2 *usbci.Magtek
-	gen1, gen2 *usbci.Generic
-
-	magChanges = make([][]string, 2)
-)
-
 func init() {
 
 	magChanges[0] = []string{`SoftwareID`, `21042818B01`, `21042818B02`}
@@ -277,7 +105,7 @@ func TestReporting(t *testing.T) {
 
 		b, err := ioutil.ReadFile(fn)
 		gotest.Ok(t, err)
-		gotest.Assert(t, mag1ShaPrettyJSON == sha256.Sum256(b), `unexpected hash signature of JSON report`)
+		gotest.Assert(t, mag1SigPrettyJSON == sha256.Sum256(b), `unexpected hash signature of JSON report`)
 	})
 
 	t.Run("XML Report", func(t *testing.T) {
@@ -290,7 +118,7 @@ func TestReporting(t *testing.T) {
 
 		b, err := ioutil.ReadFile(fn)
 		gotest.Ok(t, err)
-		gotest.Assert(t, mag1ShaPrettyXML == sha256.Sum256(b), `unexpected hash signature of XML report`)
+		gotest.Assert(t, mag1SigPrettyXML == sha256.Sum256(b), `unexpected hash signature of XML report`)
 	})
 
 	t.Run("CSV Report", func(t *testing.T) {
@@ -303,7 +131,7 @@ func TestReporting(t *testing.T) {
 
 		b, err := ioutil.ReadFile(fn)
 		gotest.Ok(t, err)
-		gotest.Assert(t, mag1ShaCSV == sha256.Sum256(b), `unexpected hash signature of CSV report`)
+		gotest.Assert(t, mag1SigCSV == sha256.Sum256(b), `unexpected hash signature of CSV report`)
 	})
 
 	t.Run("NVP Report", func(t *testing.T) {
@@ -316,7 +144,7 @@ func TestReporting(t *testing.T) {
 
 		b, err := ioutil.ReadFile(fn)
 		gotest.Ok(t, err)
-		gotest.Assert(t, mag1ShaNVP == sha256.Sum256(b), `unexpected hash signature of NVP report`)
+		gotest.Assert(t, mag1SigNVP == sha256.Sum256(b), `unexpected hash signature of NVP report`)
 	})
 
 	t.Run("Legacy Report", func(t *testing.T) {
@@ -328,7 +156,7 @@ func TestReporting(t *testing.T) {
 
 		b, err := ioutil.ReadFile(conf.Files.Legacy)
 		gotest.Ok(t, err)
-		gotest.Assert(t, mag1ShaLegacy == sha256.Sum256(b), `unexpected hash signature of Legacy report`)
+		gotest.Assert(t, mag1SigLegacy == sha256.Sum256(b), `unexpected hash signature of Legacy report`)
 	})
 
 }
@@ -450,13 +278,6 @@ func TestFileReadWrite(t *testing.T) {
 	var (
 		b []byte
 		err error
-
-		sig = [32]byte{
-			0x3a,0x39,0xc1,0x30,0x0e,0x4c,0x8b,0xdd,
-			0x90,0x04,0x1b,0xb3,0x2b,0xf6,0x2f,0x3b,
-			0x19,0x48,0xff,0xc8,0x3e,0xb5,0x59,0xf9,
-			0x03,0x8b,0xfe,0x87,0xca,0x8f,0xb4,0xe9,
-		}
 	)
 
 	// File Write Paths
@@ -471,44 +292,49 @@ func TestFileReadWrite(t *testing.T) {
 	rfn2 := `log/test2.txt`
 	rfn3 := filepath.Join(os.Getenv(`TEMP`), `test3.txt`)
 
+	// Generate file content
+
+	j, err := mag1.JSON()
+	gotest.Ok(t, err)
+
 	// File Write Tests
 
-	err = writeFile(mag1JSON, wfn1)
+	err = writeFile(j, wfn1)
 	gotest.Ok(t, err)
 
-	err = writeFile(mag1JSON, wfn2)
+	err = writeFile(j, wfn2)
 	gotest.Ok(t, err)
 
-	err = writeFile(mag1JSON, wfn3)
+	err = writeFile(j, wfn3)
 	gotest.Ok(t, err)
 
 	// File Read Tests
 
 	b, err = readFile(rfn1)
 	gotest.Ok(t, err)
-	gotest.Assert(t, sha256.Sum256(b) == sig, `unexpected hash signature of file contents`)
+	gotest.Assert(t, sha256.Sum256(b) == mag1SigJSON, `unexpected hash signature of file contents`)
 
 	b, err = readFile(rfn2)
 	gotest.Ok(t, err)
-	gotest.Assert(t, sha256.Sum256(b) == sig, `unexpected hash signature of file contents`)
+	gotest.Assert(t, sha256.Sum256(b) == mag1SigJSON, `unexpected hash signature of file contents`)
 
 	b, err = readFile(rfn3)
 	gotest.Ok(t, err)
-	gotest.Assert(t, sha256.Sum256(b) == sig, `unexpected hash signature of file contents`)
+	gotest.Assert(t, sha256.Sum256(b) == mag1SigJSON, `unexpected hash signature of file contents`)
 
 	// File Read Test Validations
 
 	b, err = ioutil.ReadFile(rfn1)
 	gotest.Ok(t, err)
-	gotest.Assert(t, sha256.Sum256(b) == sig, `unexpected hash signature of file contents`)
+	gotest.Assert(t, sha256.Sum256(b) == mag1SigJSON, `unexpected hash signature of file contents`)
 
 	b, err = ioutil.ReadFile(rfn2)
 	gotest.Ok(t, err)
-	gotest.Assert(t, sha256.Sum256(b) == sig, `unexpected hash signature of file contents`)
+	gotest.Assert(t, sha256.Sum256(b) == mag1SigJSON, `unexpected hash signature of file contents`)
 
 	b, err = ioutil.ReadFile(rfn3)
 	gotest.Ok(t, err)
-	gotest.Assert(t, sha256.Sum256(b) == sig, `unexpected hash signature of file contents`)
+	gotest.Assert(t, sha256.Sum256(b) == mag1SigJSON, `unexpected hash signature of file contents`)
 }
 
 
